7cab7731ad72193ad3bc02e4ad3430475c8ece4d
Yuriy <yurius13@ukr.net>
2024-07-30 01:09:31 +0300

    add section friends

--- src/components/FriendListItem.jsx
+++ src/components/FriendListItem.jsx
@@ -1,11 +1,17 @@
 import css from "./FriendList.module.css"
-const FriendListItem = () => {
+import clsx from "clsx";
+const FriendListItem = ({
+    avatar, name, isOnline
+}) => {
     return (
         <div>
-            <img src="" alt="Avatar" width="48" />
-            <p>Friend name</p>
-            <p>Friend status</p>
-        </div>
+            <img src={avatar} alt="Avatar" width="48" />
+            <p>{name} </p>
+            <p className={clsx(css.status, {
+                [css.online]: isOnline === true,
+                [css.offline]: isOnline === false
+            })}>{isOnline === true ? "Online" : "Offline"}</p>
+        </div >
     );
 };
 
